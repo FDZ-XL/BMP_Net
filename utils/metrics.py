3b35c95da70ead48b8257af8a3e6a4a94352d4a4
FDZ-XL <z1813914689@163.com>
2024-05-29 17:43:13 +0800

    Add files via upload

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,40 +1,100 @@
-# -*- coding: utf-8 -*-
-"""
-@Time    : 2022/11/28/028 13:45
-@Author  : NDWX
-@File    : metrics.py
-@Software: PyCharm
-"""
-import numpy as np
-import torch
-
-DEVICE = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+from . import base
+from . import functional as F
+from ..base.modules import Activation
 
 
-#  验证集不需要梯度计算,加速和节省gpu空间
-@torch.no_grad()
-# 计算验证集Iou
-def cal_val_iou(model, loader):
-    val_iou = []
-    model.eval()
-    for image, target in loader:
-        image, target = image.to(DEVICE), target.to(DEVICE)
-        output = model(image)
-        output = output.argmax(1)
-        iou = cal_iou(output, target)
-        val_iou.append(iou)
-    return val_iou
+class IoU(base.Metric):
+    __name__ = "iou_score"
+
+    def __init__(self, eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.iou(
+            y_pr,
+            y_gt,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
 
 
-# 计算IoU
-def cal_iou(pred, mask, c=2):
-    iou_result = []
-    for idx in range(c):
-        p = (mask == idx).int().reshape(-1)
-        t = (pred == idx).int().reshape(-1)
-        uion = p.sum() + t.sum()
-        overlap = (p*t).sum()
-        #  0.0001防止除零
-        iou = 2*overlap/(uion + 0.0001)
-        iou_result.append(iou.abs().data.cpu().numpy())
-    return np.stack(iou_result)
+class Fscore(base.Metric):
+    def __init__(self, beta=1, eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.beta = beta
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.f_score(
+            y_pr,
+            y_gt,
+            eps=self.eps,
+            beta=self.beta,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class Accuracy(base.Metric):
+    def __init__(self, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.accuracy(
+            y_pr,
+            y_gt,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class Recall(base.Metric):
+    def __init__(self, eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.recall(
+            y_pr,
+            y_gt,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
+
+
+class Precision(base.Metric):
+    def __init__(self, eps=1e-7, threshold=0.5, activation=None, ignore_channels=None, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+        self.threshold = threshold
+        self.activation = Activation(activation)
+        self.ignore_channels = ignore_channels
+
+    def forward(self, y_pr, y_gt):
+        y_pr = self.activation(y_pr)
+        return F.precision(
+            y_pr,
+            y_gt,
+            eps=self.eps,
+            threshold=self.threshold,
+            ignore_channels=self.ignore_channels,
+        )
